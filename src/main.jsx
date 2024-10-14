7dd324568256422451f9803ebd36e994728306e0
lelouch456 <36822032+lelouch456@users.noreply.github.com>
2024-10-15 04:57:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import ContextProvider from './context/context.jsx'
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="Gemini_Clone">
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
   <ContextProvider>
     <App />
   </ContextProvider>
